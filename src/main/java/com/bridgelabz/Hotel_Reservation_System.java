3378b1cfd11157f5566e7aae50c268f67183e51a
sandeepPangare <pangaresandeep96@gmail.com>
2022-12-12 09:13:10 +0530

    Added:Welcome to Hotel Reservation System

---
+++ src/main/java/com/bridgelabz/Hotel_Reservation_System.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class Hotel_Reservation_System {
+
+    public static void main(String[] args) {
+
+        System.out.println("___Welcome To Hotel Reservation System___");
+    }
+}
